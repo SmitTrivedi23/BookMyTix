efe5b7b13c119ed6ae51693787c9dc6b696351ad
Smit <smittrivedi23@gmail.com>
2023-08-29 08:40:40 +0530

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ import usersRoute from "./routes/usersRoute.js";
 import moviesRoute from "./routes/moviesRoute.js";
 import theatresRoute from "./routes/theatreRoute.js";
 import bookingsRoute from "./routes/bookingsRoute.js";
-import path from "path";
-import { fileURLToPath } from 'url';
+import path from "path";;
 import { dirname } from 'path';
 
 
@@ -22,8 +21,7 @@ app.use("/api/bookings", bookingsRoute);
 
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
 // render deployment
 if (process.env.NODE_ENV === "production") {
